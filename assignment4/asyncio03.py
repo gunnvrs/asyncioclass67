c9003030e80539c455b1a961258f3855a6815199
Gunn <gunn2545@gmail.com>
2024-07-05 11:44:38 +0700

    as4

--- assignment4/asyncio03.py
+++ assignment4/asyncio03.py
@@ -1,1 +1,23 @@
 # example of gather for many coroutines in a list
+import asyncio
+
+# coroutine for task
+async def task_coro(value):
+    #report a message
+    print(f'>task {value} executing')
+    #block for a moment
+    await asyncio.sleep(1)
+
+#coroutine 
+async def main():
+    #report a message
+    print('main starting')
+
+    coros = [task_coro(i) for i in range(10)]
+
+    await asyncio.gather(*coros)
+
+    print('main done')
+
+
+asyncio.run(main())
